c59d19da79fe5deed78b18fb25f07f98fdf2e84a
Alice <carrizoalice@gmail.com>
2020-10-27 20:42:23 -0300

    Updates

---
+++ precache-manifest.21d940688cde3127a992684058649384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99471de2dfe9da589e88b63943b08fe1",
+    "url": "/newfloxynews/index.html"
+  },
+  {
+    "revision": "8d9d150bcdf4a4c67dc6",
+    "url": "/newfloxynews/static/css/2.e24a89a4.chunk.css"
+  },
+  {
+    "revision": "73783190306636b1efc2",
+    "url": "/newfloxynews/static/css/main.e98961e0.chunk.css"
+  },
+  {
+    "revision": "8d9d150bcdf4a4c67dc6",
+    "url": "/newfloxynews/static/js/2.452cc708.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/newfloxynews/static/js/2.452cc708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73783190306636b1efc2",
+    "url": "/newfloxynews/static/js/main.e5ac0bdd.chunk.js"
+  },
+  {
+    "revision": "0ab98ef62ae69cab4cef",
+    "url": "/newfloxynews/static/js/runtime-main.89b3cf89.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24afdf19634c95b334076bd4f8e86bf1.js"
+  "/newfloxynews/precache-manifest.21d940688cde3127a992684058649384.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newfloxynews/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
